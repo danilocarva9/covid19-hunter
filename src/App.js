6da7474ce5ead1c0185e7e2eb51e898607a405c8
Danilo CArvalho <danilocarva9@gmail.com>
2020-04-14 12:11:05 -0400

    lunch time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './components/header';
-import 'normalize.css';
 
+import './styles.css';
+import 'normalize.css';
+import Main from './pages/main';
 const App = () => (
   <div className="App">
       <Header/>
+      <Main/>
   </div>
 );
 
