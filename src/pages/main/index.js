7950a869c05052d66a8401c4302906536a3cb75a
Danilo CArvalho <danilocarva9@gmail.com>
2020-04-15 10:24:23 -0400

    antes do bootstrap

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,7 +2,36 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+
+    state = {
+        countries: [],
+    }
+    componentDidMount(){
+        this.loadCountries();
+    }
+
+    loadCountries = async () => {
+        const response = await api.get('/summary');
+        this.setState({ countries: response.data.Countries });
+    }
+
     render(){
-        return <h1>teste</h1>;
+
+        const { countries } = this.state;
+        
+        return (
+        <div>
+        <h3>{countries.length} países com casos de covid-19.</h3>
+        
+        <div className="countries-list">
+          <ul>
+          {countries.map(country => (
+                <li key={country.Country}>{country.Country} </li>
+          ))}
+          </ul>
+        </div>
+
+        </div>
+        )
     }
 }
